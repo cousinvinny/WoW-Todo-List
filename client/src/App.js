3331bb6237d4197e8b9fb548129de1e5bd6fc2bb
Vinh Nguyen <vinhnguyensj01@gmail.com>
2023-09-08 20:31:45 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react';
+import Axios from 'axios';
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [questTitle, setQuestTitle] = useState('');
+  const [questText, setQuestText] = useState('');
+
+  const submitQuest = () => {
+    Axios.post("http://localhost:3001/api/insert", {
+      questTitle: questTitle, 
+      questText: questText,
+    }).then(() => {
+      alert('successful insert');
+    });
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <title>To Do List</title>
+      </header>
+      <div className="grid-container-whole-page">
+        <main>
+          <div className="quest-details">
+            <div className="quest-title" id="quest-title">Quest Title</div>
+            <div className="quest-summary-container">
+              <div className="quest-summary-title">Quest Summary</div>
+              <input type="text" id="quest-title-input" placeholder="Enter Quest Title" onChange={(e) => {
+                setQuestTitle(e.target.value);
+              }} />
+              <input type="text" id="quest-summary-input" placeholder="Enter Quest Text" onChange={(e) => {
+                setQuestText(e.target.value);
+              }} />
+              <p style={{ marginLeft: '0.5%' }}>Click the red button to add quest objectives</p>
+              <button id="add-objective-button">+</button> {/* Add the + icon */}
+              <div id="additional-input-container"></div>
+            </div>
+            <div className="rewards-summary-container">
+              <div className="rewards-title"></div>
+            </div>
+            <div className="accept-decline-container">
+              <button className="accept" id="accept-quest-button" onClick={submitQuest}>Accept</button>
+              <button className="decline" id="decline-quest-button">Decline</button>
+            </div>
+          </div>
+        </main>
+        <aside>
+          <div className="quest-list">
+            <div className="quest-list-title">Quests</div>
+          </div>
+        </aside>
+      </div>
+      <p>WoW To-do list</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,58 @@
+//where the express server will be running
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const mysql = require("mysql2");
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "cruddatabase",
+});
+
+
+db.getConnection((err, connection) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database');
+});
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+};
+
+app.use(cors(corsOptions));
+app.use(express.json()); //allows parsing of json request bodies
+//app.use(bodyParser.urlencoded({extended:true}));
+
+//req means request (requesting information from frontend), res means response (sending information)
+app.post("/api/insert", (req, res) => { 
+    const questTitle = req.body.questTitle;
+    const questText = req.body.questText;
+    /*const sqlInsert = "INSERT INTO quest (questTitle, questObjective, objectiveCount, objectivesCompleted, questText, reward) VALUES (?,?,?,?,?,?);";
+    db.query(sqlInsert, [id, questTitle, questObjective, objectiveCount, objectivesCompleted, questText, reward], (err,result) => {
+        console.log(request);
+    });*/
+
+    const sqlInsert = "INSERT INTO quest (questTitle, questText) VALUES (?,?);";
+    db.query(sqlInsert, [questTitle, questText], (err,result) => {
+        console.log(err);
+    });
+    
+});
+
+app.get("/api/insert", (req2, res2) =>{
+    res2.send("hello");
+});
+
+app.get("/", (req3,res3) =>{
+    res3.send("/ yee");
+});
+
+app.listen(3001, () => {
+    console.log("running on port 3001");
+});
